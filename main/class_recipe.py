40fccbe8720b09d3601a4910e9a39a80bdc44a10
wessam18 <wessamabdelnabi903@gmail.com>
2024-05-11 14:10:35 +0300

    edit

--- main/class_recipe.py
+++ main/class_recipe.py
@@ -20,13 +20,13 @@ class RecipeInfoFinder:
             response = requests.get(url, params=params)
             if response.status_code == 200:
                 data = response.json()
-                return data if data else []
+                return data if data else {}
             else:
                 print(f"Failed to retrieve data. Status code: {response.status_code}")
-                return []
+                return {}
         except requests.exceptions.RequestException as e:
             print(f"Error occurred: {e}")
-            return []
+            return {}
 
     def get_recipe_details(self, recipe_id):
         url = f"https://api.spoonacular.com/recipes/{recipe_id}/information"
@@ -37,57 +37,45 @@ class RecipeInfoFinder:
             response = requests.get(url, params=params)
             if response.status_code == 200:
                 data = response.json()
-                return data if data else None
+                return data if data else {}
             else:
                 print(f"Failed to retrieve data. Status code: {response.status_code}")
-                return None
+                return {}
         except requests.exceptions.RequestException as e:
             print(f"Error occurred: {e}")
-            return None
+            return {}
 
-if __name__ == "__main__":
-    key = "cfe4b169bfc24c4a99c6801be60b38b4"
+    def get_recipe_info(self):
+        recipe_info_dict = {}
+        nutrient_recipes = self.find_recipe_by_nutrients()
+        if nutrient_recipes:
+            for recipe in nutrient_recipes:
+                recipe_id = recipe.get('id')
+                recipe_details = self.get_recipe_details(recipe_id)
+                if recipe_details:
+                    recipe_info = {
+                        "title": recipe_details.get('title'),
+                        "image": recipe_details.get('image'),
+                        "calories": recipe_details.get('calories'),
+                        "total_fat": recipe_details.get('fat'),
+                        "protein": recipe_details.get('protein'),
+                        "carbohydrate": recipe_details.get('carbs'),
+                        "cholesterol": recipe_details.get('cholesterol'),
+                        "ready_in_minutes": recipe_details.get('readyInMinutes'),
+                        "servings": recipe_details.get('servings'),
+                        "dish_type": recipe_details.get('dishTypes', [])[0] if recipe_details.get('dishTypes') else None,
+                        "summary": recipe_details.get('summary'),
+                        "ingredients": [ingredient.get('name', '') for ingredient in recipe_details.get('extendedIngredients', [])],
+                        "instructions": []
+                    }
+                    instruction_data = recipe_details.get('analyzedInstructions', [])
+                    for section in instruction_data:
+                        steps = section.get('steps', [])
+                        for step in steps:
+                            step_number = step.get('number', '')
+                            instruction = step.get('step', '')
+                            if step_number and instruction:
+                                recipe_info["instructions"].append(f"Step {step_number}: {instruction}")
+                    recipe_info_dict[recipe_id] = recipe_info
 
-    # Create an instance of RecipeFinder
-    recipe_finder = RecipeInfoFinder(api_key=key)
-    
-    # Find recipes based on nutrients
-    nutrient_recipes = recipe_finder.find_recipe_by_nutrients()
-    if nutrient_recipes:
-        for recipe in nutrient_recipes:
-            recipe_id = recipe.get('id')
-            
-            # Get detailed information for each recipe
-            recipe_details = recipe_finder.get_recipe_details(recipe_id)
-            if recipe_details:
-                print(f"Recipe Name: {recipe_details.get('title')}")
-                print(f"Image: {recipe_details.get('image')}")
-                print(f"Calories: {recipe_details.get('calories')}")
-                print(f"Total Fat: {recipe_details.get('fat')}")
-                print(f"Protein: {recipe_details.get('protein')}")
-                print(f"Carbohydrate: {recipe_details.get('carbs')}")
-                print(f"Cholesterol: {recipe_details.get('cholesterol')}")
-                print(f"Ready in Minutes: {recipe_details.get('readyInMinutes')}")
-                print(f"Number Of Serving People: {recipe_details.get('servings')}")
-                dish_types = recipe_details.get('dishTypes', [])
-                if dish_types:
-                    print(f"Dish Type: {dish_types[0]}")
-                print(f"Summary: {recipe_details.get('summary')}")
-                
-                # Output the ingredients
-                print("Ingredients:")
-                ingredient_data = recipe_details.get('extendedIngredients', [])
-                for ingredient in ingredient_data:
-                    ingredient_name = ingredient.get('name', '')
-                    print(f"- {ingredient_name}")
-                
-                # Output the instructions
-                print("Instructions:")
-                instruction_data = recipe_details.get('analyzedInstructions', [])
-                for section in instruction_data:
-                    steps = section.get('steps', [])
-                    for step in steps:
-                        step_number = step.get('number', '')
-                        instruction = step.get('step', '')
-                        if step_number and instruction:
-                            print(f"Step {step_number}: {instruction}")
+        return recipe_info_dict
